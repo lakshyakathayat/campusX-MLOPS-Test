cf959088abb4dc961ca9d090a3a613d3b218a307
lakshyakathayat <lakshyakathayat@gmail.com>
2026-02-04 01:23:46 +0100

    cookiecutter intial commit

---
+++ swiggy-top3-recco/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='an service recommending top 3 dishes',
+    author='lakshya kathayat',
+    license='',
+)
